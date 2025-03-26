ba663222348a0a422a4fd3d4eec5519f6f9c603f
Tudor Ambarus <tudordana@google.com>
2025-03-26 13:10:30 +0000

    bigo: convert symbol namespace to string literal (v6.13 compatibility)
    Upstream commit cdd30ebb1b9f ("module: Convert symbol namespace to string literal")
    cleaned the existing export namespace code as it is not desired for the namespace
    argument to be a macro expansion itself. This caused the OOT pixel drivers to fail
    compilation. Convert symbol namespace to string literal.
    
    Bug: 406450813
    Test: TH
    Fixes: cdd30ebb1b9f ("module: Convert symbol namespace to string literal")
    Change-Id: Iec79daeb59eb501c804023c528222111fd105064
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- bigo.c
+++ bigo.c
@@ -895,4 +895,4 @@ module_platform_driver(bigo_driver);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Vinay Kalia <vinaykalia@google.com>");
 MODULE_DESCRIPTION("BigWave driver");
-MODULE_IMPORT_NS(DMA_BUF);
+MODULE_IMPORT_NS("DMA_BUF");
